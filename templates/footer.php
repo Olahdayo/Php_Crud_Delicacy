8d56150133a2327f87735d0feedc8287f5c3d2cb
Oladayo <blessinoloyede@gmail.com>
2024-11-25 20:56:13 +0100

    Genesis Commit:Php Crud functionality with delicacies

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark text-white py-3 mt-auto">
+    <div class="container text-center">
+        <p class="mb-0">
+            &copy; Copyright DevDY!!! 
+        </p>
+    </div>
+</footer>
